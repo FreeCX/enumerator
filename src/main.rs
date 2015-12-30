29ee945309d55beac43786d55e8074431d7c5a92
Alexey Golubev <dr.freecx@gmail.com>
2015-12-30 15:52:59 +0300

    - use Numeric from executor module - add code for ignoring null strings

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 mod tokenizer;
 mod executor;
 use tokenizer::*;
-use executor::execute;
+use executor::{execute, Numeric};
 use std::collections::HashMap;
 use std::io::prelude::*;
 use std::io;
 
 fn main() {
     // contain all program variables
-    let mut variables: HashMap<String, f32> = HashMap::new();
+    let mut variables: HashMap<String, executor::Numeric> = HashMap::new();
     // string to execute
     let mut buffer = String::new();
     loop {
@@ -19,6 +19,10 @@ fn main() {
         io::stdin().read_line(&mut buffer)
             .ok()
             .expect( "[error] Can't read line from stdin!" );
+        // ignore null strings
+        if buffer.trim().len() == 0 {
+            continue;
+        }
         // split string to tokens
         let data = tokenizer::tokenize(buffer.trim());
         // execute operation (check by exit flag)
